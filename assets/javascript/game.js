49e706cddee1484bcf0039ab28009212a85240ce
HumanJBooF <humanjboof@gmail.com>
2018-06-29 22:49:48 -0400

    added restart function and winlose function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,8 +7,8 @@ $(function() {
 
 
     $('.current').text(randomNum);
-    $('.wins').text(wins);
-    $('.losses').text(losses);
+    $('.wins').text("wins: " + wins);
+    $('.losses').text("losses: " + losses);
     $('.score').text(userNum);
 
     var num1= Math.floor(Math.random() * 11+1)
@@ -17,23 +17,69 @@ $(function() {
     var num4= Math.floor(Math.random() * 11+1)
 
 
+
+
+
+function restart(){
+    randomNum = Math.floor(Math.random() * 102+19);
+    $('.current').text(randomNum);
+    num1 = Math.floor(Math.random() * 11+1);
+    num2 = Math.floor(Math.random() * 11+1);
+    num3 = Math.floor(Math.random() * 11+1);
+    num4 = Math.floor(Math.random() * 11+1);
+    userNum = 0;
+    $('.score').text(userNum);
+
+} 
+
+function winLose(){
+    if(userNum == randomNum){
+        wins++;
+        $('.wins').text(wins);
+        restart();
+    }
+   
+    else if( userNum > randomNum){
+        losses++;
+        $('.losses').text(losses);
+        restart();
+    
+    }
+
+
+    
+}
+
+
+
+
+
     $('#one').on('click', function(){
         userNum = userNum + num1;
         console.log(userNum);
-    })
+        $('.score').text(userNum);  
+        winLose();
+     })
 
     $('#two').on('click',function(){
         userNum = userNum + num2;
         console.log(userNum);
+        $('.score').text(userNum);
+        winLose();
     })
 
     $("#three").on('click',function(){
         userNum = userNum + num3;
         console.log(userNum);
+        $('.score').text(userNum);
+        winLose();
     })
 
     $('#four').on('click',function(){
         userNum = userNum + num4;
+        console.log(userNum)
+        $('.score').text(userNum);
+        winLose();
     })
    
 });
