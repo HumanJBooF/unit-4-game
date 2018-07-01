536bc4ec9eaa12468396f161a87d1082926667fe
HumanJBooF <humanjboof@gmail.com>
2018-06-30 23:06:03 -0400

    yes! js is working now, better set-up! style time!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,105 +4,62 @@ $(function () {
     var losses = 0;
     var wins = 0;
     var score = 0;
-    var images = ["./assets/images/Akuma1.png","assets/images/pfken.png","assets/images/vega.png","assets/images/ryusf.jpg"];
-
-    randomNum = Math.floor(Math.random() * 102 + 19);
+    var images = ["./assets/images/Akuma1.png", "assets/images/pfken.png", "assets/images/vega.png", "assets/images/ryusf.jpg"];
 
 
-    $('.current').text(randomNum);
-    $('.wins').text("wins: " + wins);
-    $('.losses').text("losses: " + losses);
-    $('.score').text(score);
-
-    
-
-
-    for(var i = 0; i < images.length; i++) {
-        var fighters = $('<img>');
-        fighters.addClass('fighters');
-        fighters.attr('src', images[i]);
-        fighters.attr('value', Math.floor(Math.random() * 11 + 1));
-        $('.gems').append(fighters);
-        
+    //put the current random number to match into a function
+    function randomNumfun() {
+        randomNum = Math.floor(Math.random() * 102 + 19); //current randomly generated number
     }
 
+    //this was my attempt at adding images and giving them a random value from 1 - 12.. 
+    //thinking theres an easier way but this is better then rewriting math.floor 4 times
+    function fighterS() {
+        for (var i = 0; i < images.length; i++) {
+            var fighters = $('<img>');
+            fighters.addClass('fighters');
+            fighters.attr('src', images[i]);
+            fighters.attr('value', Math.floor(Math.random() * 12 + 1));
+            $('.gems').append(fighters);
+        }
+    }
 
-    
+    //function to reset all text on screen
+    function resetText() {
+        $('.current').text(randomNum);
+        $('.wins').text("wins: " + wins);
+        $('.losses').text("losses: " + losses);
+        $('.score').text('Your score is: ');
+        $('.fighters').empty();
+    }
 
-    $('.fighters').on("click", function() {
-        score += parseInt($(this).attr('value'));
-        console.log(score);
-        $('.score').text(score);
-    })
+    //reset the random number and the score
+    function resetAll() {
+        randomNumfun();
+        score = 0;
+        resetText();
 
+    }
+    //Game Start 
+    randomNumfun();
+    resetText();
+    fighterS();
 
+    //takes the value from a fighter and turns it into an integer
+    function fighterClick() {
+        score += parseInt($(this).attr("value"));
+        $(".score").text('Your score is: ' + score);
+        if (score == randomNum) {
+            wins++;                 //If your score equals the randomly generated number you win!
+            resetAll();
+        }
+        else if (score > randomNum) {
+            losses++;               //if you go over that number you lose!
+            resetAll();
+        };
+    }
 
-    // var num1 = Math.floor(Math.random() * 11 + 1)
-    // var num2 = Math.floor(Math.random() * 11 + 1)
-    // var num3 = Math.floor(Math.random() * 11 + 1)
-    // var num4 = Math.floor(Math.random() * 11 + 1)
-
-    // function restart() {
-    //     randomNum = Math.floor(Math.random() * 102 + 19);
-    //     $('.current').text(randomNum);
-      
-      
-      
-    //     // num1 = Math.floor(Math.random() * 11 + 1);
-    //     // num2 = Math.floor(Math.random() * 11 + 1);
-    //     // num3 = Math.floor(Math.random() * 11 + 1);
-    //     // num4 = Math.floor(Math.random() * 11 + 1);
-    //     userNum = 0;
-    //     $('.score').text(userNum);
-        
-
-    // }
-
-    // function winLose() {
-    //     if (userNum == randomNum) {
-    //         wins++;
-    //         $('.wins').text("wins: " + wins);
-    //         restart();
-    //     }
-
-    //     else if (userNum > randomNum) {
-    //         losses++;
-    //         $('.losses').text("losses: " + losses);
-    //         restart();
-    //     }
-
-       
-       
-       
-
-    // }
-
-
-    // $('#one').on('click', function () {
-    //     // userNum = userNum + num1;
-    //     // $('.score').text(userNum);
-    //     winLose();
-    // })
-
-    // $('#two').on('click', function () {
-    //     userNum = userNum + num2;
-    //     console.log(userNum);
-    //     $('.score').text(userNum);
-    //     winLose();
-    // })
-
-    // $("#three").on('click', function () {
-    //     userNum = userNum + num3;
-    //     console.log(userNum);
-    //     $('.score').text(userNum);
-    //     winLose();
-    // })
-
-    // $('#four').on('click', function () {
-    //     userNum = userNum + num4;
-    //     console.log(userNum)
-    //     $('.score').text(userNum);
-    //     winLose();
-    // })
+    //tried to make ,fighters work but after some research this was the way to get it all to come together
+    $(document).on("click", '.fighters', fighterClick);
 
 });
