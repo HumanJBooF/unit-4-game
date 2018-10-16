fe3ced4df7255d383b83322f01211de16664f87c
humanjboof <humanjboof@gmail.com>
2018-10-16 16:35:24 -0400

    es6

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,74 +1,64 @@
 $(function () {
 
-    var randomNum = "";
-    var losses = 0;
-    var wins = 0;
-    var score = 0;
-    var images = ["./assets/images/ryunobg.png", "assets/images/veganobg.png", "assets/images/sagatnobg.png", "assets/images/kumanobgpng.png"];
-    
+    const images = ["./assets/images/ryunobg.png", "assets/images/veganobg.png", "assets/images/sagatnobg.png", "assets/images/kumanobgpng.png"];
+    const $current = $('.current');
+    const $wins = $('.wins');
+    const $losses = $('.losses');
+    const $score = $('.score');
+    const $listGroup = $('.list-group');
+    let randomNum = "";
+    let losses = 0;
+    let wins = 0;
+    let score = 0;
 
     //put the current random number to match into a function
-    function randomNumfun() {
+    const randomNumfun = () => {
         randomNum = Math.floor(Math.random() * 102 + 19); //current randomly generated number
     }
 
-//adding images to html with a random value of 1-12
-    function fighterS() {
-       
-        for (var i = 0; i < images.length; i++) {
-            var fighters = $('<img>');
+    //adding images to html with a random value of 1-12
+    const fighterS = () => {
+        images.forEach(image => {
+            const fighters = $('<img>');
             fighters.addClass('fighters');
-            fighters.attr('src', images[i]);
+            fighters.attr('src', image);
             fighters.attr('value', Math.floor(Math.random() * 12 + 1));
             $('.gems').append(fighters);
-        }
-    }
-
-    //function to reset all text on screen
-    function resetText() {
-        $('.current').text("Target: " + randomNum);
-        $('.wins').text("Wins: " + wins);
-        $('.losses').text("Losses: " + losses);
-        $('.score').text('Your score is: ');
-        $('.fighters').empty();
+        });
     }
 
     //reset the random number and the score
-    function resetAll() {
+    const resetAll = () => {
         randomNumfun();
         score = 0;
-        resetText();
-
+        $current.text(`Target:  ${randomNum}`);
+        $wins.text(`Wins: ${wins}`);
+        $losses.text(`Losses: ${losses}`);
+        $score.text(`Your score is: `);
+        $('.fighters').empty();
     }
-   
-
 
     //Game Start 
     randomNumfun();
-    resetText();
+    resetAll();
     fighterS();
 
     //takes the value from a fighter and turns it into an integer
-    function fighterClick() {
+    function fighterClick () {
         score += parseInt($(this).attr("value"));
-        $(".score").text('Your score is: ' + score);
-        if (score == randomNum) {
+        $(".score").text(`Your score is: ${score}`);
+        if (score === randomNum) {
             wins++;                //If your score equals the randomly generated number you win!
             resetAll();
-        }
-        else if (score > randomNum) {
-            losses++;               //if you go over that number you lose!
+        } else if (score > randomNum) {     //if you go over that number you lose!
+            losses++;
             resetAll();
         };
     }
-       
-
-
 
     //click event!
     $(document).on("click", '.fighters', fighterClick);
-        
-   
-
-
+    $listGroup.on('click', () => {
+        $listGroup.fadeOut();
+    })
 });
